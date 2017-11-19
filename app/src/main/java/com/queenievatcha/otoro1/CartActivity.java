f4067d2f682a1f200fc56224eb566d8cfb6b5ca4
queenievatcha <boonsita.queen@gmail.com>
2017-11-19 16:24:09 +0700

    Initial commit

---
+++ app/src/main/java/com/queenievatcha/otoro1/CartActivity.java
@@ -0,0 +1,13 @@
+package com.queenievatcha.otoro1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
---
+++ app/src/main/java/com/queenievatcha/otoro1/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.queenievatcha.otoro1;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
