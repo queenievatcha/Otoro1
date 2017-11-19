d8f69c606a718e6b7092edcd2fc94130298154b6
queenievatcha <boonsita.queen@gmail.com>
2017-11-19 21:53:17 +0700

    Updated HomeActivity

--- app/src/main/java/com/queenievatcha/otoro1/HomeActivity.java
+++ app/src/main/java/com/queenievatcha/otoro1/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.queenievatcha.otoro1;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +9,42 @@ import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button testCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+        }
     }
 
     public void goCart(View v) {
-
         Intent in = new Intent(HomeActivity.this, CartActivity.class);
         startActivity(in);
-
-
     }
+
+    public void goHistory(View v) {
+        Intent in = new Intent(HomeActivity.this, HistoryActivity.class);
+        startActivity(in);
+    }
+
+    public void goMenu(View v) {
+        Intent in = new Intent(HomeActivity.this, MenuActivity.class);
+        startActivity(in);
+    }
+
+    public void goMap(View v) {
+        Intent in = new Intent(HomeActivity.this, MapActivity.class);
+        startActivity(in);
+    }
+
+    public void popUp (View v){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(HomeActivity.this);
+        dialog.setTitle("Delivery Schedule");
+        dialog.setMessage("eflgsjopg[ikgs'epgj'rgj;ho");
+        dialog.show();
+    }
+
 }
