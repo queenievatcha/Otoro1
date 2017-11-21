67a3a69d146a7ec85befcad5e5eb74b7aa830270
Inkinkz <nithiphorn.n@gmail.com>
2017-11-21 16:32:18 +0700

    added xml for previous commit

---
+++ app/src/main/java/com/queenievatcha/otoro1/CustomAdapterCart.java
@@ -0,0 +1,119 @@
+package com.queenievatcha.otoro1;
+
+/**
+ * Created by inkz on 21/11/2017 AD.
+ */
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+
+public class CustomAdapterCart extends ArrayAdapter{
+    private Activity context;
+    int pos;
+    ArrayList<String> foodAL = new ArrayList<String>();
+    ArrayList<String> minusAL = new ArrayList<String>();
+    ArrayList<String> plusAL = new ArrayList<String>();
+    ArrayList<Integer> imgIDAL = new ArrayList<Integer>();
+    ArrayList<Integer> amountAL = new ArrayList<Integer>();
+
+
+    CustomAdapterCart(@NonNull Activity context, ArrayList<String> food, ArrayList<Integer> imgID, ArrayList<String> butPlus, ArrayList<String> butMinus, ArrayList<Integer> amount) {
+        super(context, R.layout.custom_row2, food);
+        this.context = context;
+        this.foodAL = food;
+        this.imgIDAL = imgID;
+        this.plusAL = butPlus;
+        this.minusAL = butMinus;
+        this.amountAL = amount;
+    }
+
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View r = convertView;
+        ViewHolder viewHolder = null;
+        if (r == null) {
+            LayoutInflater layoutInflater = context.getLayoutInflater();
+            r = layoutInflater.inflate(R.layout.custom_row2, null, true);
+            viewHolder = new ViewHolder(r);
+            r.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) r.getTag();
+        }
+        viewHolder.foodImage.setImageResource(imgIDAL.get(position));
+        viewHolder.foodName.setText(foodAL.get(position));
+        viewHolder.foodButtonPlus.setText(plusAL.get(position));
+        viewHolder.foodButtonMinus.setText(minusAL.get(position));
+        viewHolder.amountDisp.setText(Integer.toString(amountAL.get(position)));
+        return r;
+    }
+
+    class ViewHolder {
+        TextView foodName;
+        ImageView foodImage;
+        Button foodButtonPlus;
+        Button foodButtonMinus;
+        TextView amountDisp;
+        int count;
+
+        ViewHolder(View v) {
+            foodName = v.findViewById(R.id.foodName);
+            foodImage = v.findViewById(R.id.foodImage);
+            amountDisp = v.findViewById(R.id.amountDisp);
+            foodButtonPlus = v.findViewById(R.id.foodButtonPlus);
+            foodButtonPlus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    count = amountAL.get(pos);
+                    count = count + 1;
+                    if ((foodName.getText() + "").equalsIgnoreCase("burger"))
+                        pos = 0;
+                    if ((foodName.getText() + "").equalsIgnoreCase("crab"))
+                        pos = 1;
+                    if ((foodName.getText() + "").equalsIgnoreCase("fish"))
+                        pos = 2;
+                    if ((foodName.getText() + "").equalsIgnoreCase("pizza"))
+                        pos = 3;
+                    if ((foodName.getText() + "").equalsIgnoreCase("shrimp"))
+                        pos = 4;
+                    MenuActivity.addAmount(pos);
+                    amountDisp.setText(Integer.toString(count));
+                    foodButtonMinus.setEnabled(true);
+                }
+            });
+
+            foodButtonMinus = v.findViewById(R.id.foodButtonMinus);
+            foodButtonMinus.setEnabled(false);
+            foodButtonMinus.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    count = count - 1;
+                    if ((foodName.getText() + "").equalsIgnoreCase("burger"))
+                        pos = 0;
+                    if ((foodName.getText() + "").equalsIgnoreCase("crab"))
+                        pos = 1;
+                    if ((foodName.getText() + "").equalsIgnoreCase("fish"))
+                        pos = 2;
+                    if ((foodName.getText() + "").equalsIgnoreCase("pizza"))
+                        pos = 3;
+                    if ((foodName.getText() + "").equalsIgnoreCase("shrimp"))
+                        pos = 4;
+                    MenuActivity.minusAmount(pos);
+                    amountDisp.setText(Integer.toString(count));
+                    if (count == 0) {
+                        foodButtonMinus.setEnabled(false);
+                    }
+                }
+            });
+        }
+
+    }
+
+}
